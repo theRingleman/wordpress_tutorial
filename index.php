8e60eaad2b0e02c216f9e097450cd1cae00dbab1
theRingleman <sam.ringleman@gmail.com>
2016-10-15 10:01:37 -0600

    add some columns to the index

--- index.php
+++ index.php
@@ -5,5 +5,19 @@
     <h1 class="text-center">Welcome to my practice WordPress theme!</h1>
   </div>
 
+  <div class="row">
+    <div class="col-sm-4">
+      <p>This is my first awesome column</p>
+    </div>
+
+    <div class="col-sm-4">
+      <p>And this here is my second awesome column!</p>
+    </div>
+
+    <div class="col-sm-4">
+      <p>This is my sick sidebar!</p>
+    </div>
+  </div>
+
 
 <?php get_footer(); ?>
