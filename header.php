8dda26eeb89f5255d85143d1c43f2c03d86c1f89
theRingleman <sam.ringleman@gmail.com>
2016-10-14 19:18:38 -0600

    first commit

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php
+  wp_head();
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo bloginfo("title") ?></title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url') ?>">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+
+  <div class="container">
+    <h1 class="text-center">Welcome to my practice WordPress theme!</h1>
+  </div>
+
+
+<?php get_footer(); ?>
