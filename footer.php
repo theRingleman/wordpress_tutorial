c8b095ff234b70531cec996493be30a781e14ccb
theRingleman <sam.ringleman@gmail.com>
2016-10-15 10:11:51 -0600

    add some content to the footer

--- footer.php
+++ footer.php
@@ -2,6 +2,10 @@
   wp_footer();
  ?>
 
+ <footer>
+   <p class="text-center">&copy; <?php echo get_current_year(); ?> Designed by Sam Ringleman</p>
+ </footer>
+
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
