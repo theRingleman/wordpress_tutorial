be36902895c0e645574a7af84ef20f4d2b1e68e5
theRingleman <sam.ringleman@gmail.com>
2016-10-17 14:58:48 -0600

    add post folder and changed the name of the layout so I can organize this code better

--- loop.php
+++ loop.php
@@ -1,7 +1,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
   <?php if (get_post_type() == "post" ) {
-      get_template_part("post_layout");
+      get_template_part("post/layout");
   } ?>
 
 <?php endwhile; else : ?>
