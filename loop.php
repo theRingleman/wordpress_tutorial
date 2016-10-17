6971278bbde56b986a4972ae3eb1fbf9342cc8f7
theRingleman <sam.ringleman@gmail.com>
2016-10-17 15:39:49 -0600

    add some temp html for the page layout

--- loop.php
+++ loop.php
@@ -1,8 +1,14 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  <?php if (get_post_type() == "post" ) {
+  <?php
+
+    if (get_post_type() == "post" ) {
       get_template_part("post/layout");
-  } ?>
+    } elseif (get_post_type() == "page") {
+      get_template_part("page/layout");
+    }
+
+  ?>
 
 <?php endwhile; else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
