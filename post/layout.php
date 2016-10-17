f5e9b462df97297f397077bc0c17038f238abb1d
theRingleman <sam.ringleman@gmail.com>
2016-10-17 15:01:58 -0600

    extract even more out of the single.php and add custom coding into the post layout

--- post/layout.php
+++ post/layout.php
@@ -1,6 +1,15 @@
-<?php
-  echo "<h1>" . $post->post_title . "</h1>";
-  echo "<p>" . the_date() . "</p>";
-  echo "<p>" . the_category(", ") . "</p>";
-  the_content();
- ?>
+<div class="wrapper">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-8">
+        <?php
+          echo "<h1>" . $post->post_title . "</h1>";
+          echo "<p>" . the_date() . "</p>";
+          echo "<p>" . the_category(", ") . "</p>";
+          the_content();
+         ?>
+      </div>
+      <?php get_sidebar(); ?>
+    </div>
+  </div>
+</div>
