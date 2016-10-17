8b1f3bd56de8899c99d8f6baa042b877c8eaa6ba
theRingleman <sam.ringleman@gmail.com>
2016-10-17 11:36:00 -0600

    add bootstrap sass and compass and pull my hair out in the process

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
 <head>
   <meta charset="UTF-8">
   <title><?php echo bloginfo("title") ?></title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo bloginfo('stylesheet_url') ?>">
 </head>
 <body>
