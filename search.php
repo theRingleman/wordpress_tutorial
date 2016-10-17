d2e244b12ecdd05e4e48f4db08b15901ed25597d
theRingleman <sam.ringleman@gmail.com>
2016-10-17 17:41:16 -0600

    add search template and initial html

---
+++ search.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <h2 class="text-center">Search Results: <?php echo get_search_query(); ?></h2>
+      </div>
+    </div>
+  </div>
+<?php get_footer(); ?>
