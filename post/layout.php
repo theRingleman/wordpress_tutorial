1b185edc4ae7eb7313ed084b8778a0c9ecd7d3a0
theRingleman <sam.ringleman@gmail.com>
2016-10-17 16:15:22 -0600

    add comment template to posts

--- post/layout.php
+++ post/layout.php
@@ -7,6 +7,7 @@
           echo "<p>" . the_date() . "</p>";
           echo "<p>" . the_category(", ") . "</p>";
           the_content();
+          comments_template();
          ?>
       </div>
       <?php get_sidebar(); ?>
