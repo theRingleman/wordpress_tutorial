947c91e3b1af37e0ca64cb206401558da6b805cc
theRingleman <sam.ringleman@gmail.com>
2016-10-17 14:19:32 -0600

    add some content and styles to the post template

--- loop.php
+++ loop.php
@@ -1,7 +1,10 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-  <?php the_title(); ?>
-  <?php the_content(); ?>
+  <?php if (get_post_type() == "post" ) {
+      echo "<h1>" . $post->post_title . "</h1>";
+      echo "<p>" . the_date() . "</p>";
+      the_content();
+  } ?>
 
 <?php endwhile; else : ?>
 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
