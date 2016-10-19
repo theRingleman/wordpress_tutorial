3b1faba6d2ec0dbf1574159a2cc2f11658085bc7
theRingleman <sam.ringleman@gmail.com>
2016-10-19 12:22:14 -0600

    add some code to the search page

--- search.php
+++ search.php
@@ -1,8 +1,25 @@
+<?php
+/*
+Template Name: Search Page
+*/
+?>
+
 <?php get_header(); ?>
   <div class="container">
     <div class="row">
       <div class="col-sm-12">
         <h2 class="text-center">Search Results: <?php echo get_search_query(); ?></h2>
+
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          <br>
+          <?php the_excerpt(); ?>
+          <hr>
+
+        <?php endwhile; else : ?>
+        	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+        <?php endif; ?>
       </div>
     </div>
   </div>
