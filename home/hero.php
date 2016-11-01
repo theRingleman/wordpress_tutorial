6ab6d7e51e49c70d973db7d011cc3f52a2e19574
theRingleman <sam.ringleman@gmail.com>
2016-11-01 15:53:15 -0600

    finish up the hero section

--- home/hero.php
+++ home/hero.php
@@ -1,3 +1,8 @@
-<div class="container hero">
+<section class="hero row">
   <?php get_template_part("navigation"); ?>
-</div>
+
+  <div class="hero-logo">
+    <h1>Welcome To</h1>
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-white.png" alt="" class="center-block">
+  </div>
+</section>
--- navigation.php
+++ navigation.php
@@ -1,24 +1,26 @@
-<nav class="navbar navbar-default" role="navigation">
-<!-- Brand and toggle get grouped for better mobile display -->
-  <div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-      <span class="sr-only">Toggle navigation</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>
-  </div>
-  <!-- Collect the nav links, forms, and other content for toggling -->
-  <div class="collapse navbar-collapse navbar-ex1-collapse">
-    <?php /* Primary navigation */
-        wp_nav_menu( array(
-      'menu'       => 'primary',
-      'depth'      => 2,
-      'container'  => false,
-      'menu_class' => 'nav navbar-nav navbar-right',
-      'walker'     => new wp_bootstrap_navwalker())
-      );
-    ?>
-  </div>
-</nav>
+<div class="container">
+  <nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php bloginfo('url')?>"><?php bloginfo('name')?></a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+      <?php /* Primary navigation */
+          wp_nav_menu( array(
+        'menu'       => 'primary',
+        'depth'      => 2,
+        'container'  => false,
+        'menu_class' => 'nav navbar-nav navbar-right',
+        'walker'     => new wp_bootstrap_navwalker())
+        );
+      ?>
+    </div>
+  </nav>
+</div>
