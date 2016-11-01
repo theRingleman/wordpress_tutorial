62ca4f033014ada70d1bbc7f0cb0b0d2830e416d
theRingleman <sam.ringleman@gmail.com>
2016-11-01 16:32:30 -0600

    add styles to the heading and add background to the story section

--- home/our_story.php
+++ home/our_story.php
@@ -1,9 +1,7 @@
-<section class="container our-story">
-  <div class="row">
-    <div class="col-sm-4 col-sm-offset-1">
+<section class="our-story row">
+    <div class="col-sm-4 col-sm-offset-2">
       <h2><span class="cursive">Learn</span> our story</h2>
       <p>The history of The Hideaway cafe is a long and intriguing one.  First opened in 1988 by itës founder Jeff Yeh, The Hideaway cafe prides itself in providing quality food and service.  One of the proudest achievements of The Hideaway coffee and cafe was winning the gold medal for the 2016 annual coffe conferences.  With a wide variety of food and drinks, The Hideaway is sure to sate anyoneës hunger.  Vegetarian and gluten free options are available.</p>
       <button>Learn More</button>
     </div>
-  </div>
 </section>
