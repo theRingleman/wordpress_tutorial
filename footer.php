e62d380e596c4a4c6cc64aee53fc8a4575afd48c
theRingleman <sam.ringleman@gmail.com>
2016-11-02 16:57:14 -0600

    add some more text to the footer and start the widget process

--- footer.php
+++ footer.php
@@ -4,9 +4,34 @@
 
   <footer class="footer container-fluid">
     <div class="row">
-      <div class="col-sm-4 col-sm-offset-2">
+      <div class="col-sm-3 col-sm-offset-2">
         <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="Hideaway Cafe">
       </div>
+
+      <div class="col-sm-2">
+        <p class="footer-heading">Hideaway Cafe</p>
+        <p>home</p>
+        <p>our story</p>
+        <p>menu</p>
+        <p>contact</p>
+      </div>
+
+      <div class="col-sm-2">
+        <p class="footer-heading">Social Media</p>
+        <p>Facebook</p>
+        <p>Google</p>
+        <p>Twitter</p>
+        <p>Foursquare</p>
+      </div>
+
+      <div class="col-sm-2">
+        <p class="footer-heading">Find Us</p>
+        <p>(970) 555-5555</p>
+        <p>123 Downtown Dr</p>
+        <p>Loveland CO 80538</p>
+        <p>contact@hideawaycafe.com</p>
+      </div>
+
     </div>
   </footer>
 
--- functions.php
+++ functions.php
@@ -19,6 +19,20 @@
         }
     endif;
 
+    if (function_exists('register_sidebar')) {
+
+  	register_sidebar(array(
+  		'name' => 'Widgetized Area',
+  		'id'   => 'widgetized-area',
+  		'description'   => 'This is a widgetized area.',
+  		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+  		'after_widget'  => '</div>',
+  		'before_title'  => '<h4>',
+  		'after_title'   => '</h4>'
+  	));
+
+  }
+
   function get_current_year(){
     $this_year = Date("Y");
     return $this_year;
