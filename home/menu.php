af50a5205efc5e85626c459d1d6344562e5c8a71
theRingleman <sam.ringleman@gmail.com>
2016-11-02 11:46:56 -0600

    add the menu text and set up the list for proper display

--- home/menu.php
+++ home/menu.php
@@ -1,3 +1,36 @@
-<section class="row menu">
-  
+<section class="row menu-section">
+  <h2 class="text-center section-heading"><span class="cursive menu">Dining</span> Menu</h2>
+  <h3 class="text-center">All Day Breakfast</h3>
+
+  <div class="col-sm-4 col-sm-offset-2">
+    <dl>
+      <dt>The Hideaway Breakfast <span class="menu-price">$21.00</span></dt>
+      <dd>Tender lean steak, smoked bacon, sausages, golden hash brown, freerange eggs scrambled or poached, roasted tomato and sauteed mushrooms. Served with Toast</dd>
+
+      <dt>Hideaway Pancake Stack<span class="menu-price">$14.50</span></dt>
+      <dd>Fluffy pancakes drizzled in maple syrup, served with vanilla or chocolate icecream and cream on top.</dd>
+
+      <dt>Gourmet Burger<span class="menu-price">$20.00</span></dt>
+      <dd>Prime Angus beef pattie, lettuce, tomato, red onian, cheddar and freshly made aioli.  Served with cajun chips and salad.</dd>
+
+      <dt>Chicken Schnitzel &amp; Chips<span class="menu-price">$12.00</span></dt>
+      <dd>Succulent chicken coated in breadcrumbs, cooked until golden.  Served with sliced avocado, tomato, lettuce, chips and fresh aioli.</dd>
+    </dl>
+  </div>
+
+  <div class="col-sm-4 col-sm-offset-1">
+    <dl>
+      <dt>Bacon, Eggs &amp; Toast<span class="menu-price">$10.00</span></dt>
+      <dd>Smoked bacon and freerange eggs, scrambled or poached, with roasted tomato, served with toasted ciabatta.</dd>
+
+      <dt>Eggs benedict<span class="menu-price">$8.50</span></dt>
+      <dd>Fresh freerange eggs served on toasted ciabatta topped with rich hollandaise sauce.</dd>
+
+      <dt>Potato wedges<span class="menu-price">$3.50</span></dt>
+      <dd>Crispy potato wedges, fried until golden, seasoned with premium herbs and spices.  Served with fresh aioli adn salad.</dd>
+
+      <dt>Hideaway Fish &amp; Chips<span class="menu-price">$26.50</span></dt>
+      <dd>Fresh hoki fillets encased in a light, crunchy beer batter.  Served with your choice of chips or wedges, salad and a zesty tartare sauce.</dd>
+    </dl>
+  </div>
 </section>
