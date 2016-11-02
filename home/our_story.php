3d77c57e6ca8fc0ecaed5483582d7232cf6676fd
theRingleman <sam.ringleman@gmail.com>
2016-11-02 10:47:42 -0600

    add the cupcake thumbnail to the story section

--- home/our_story.php
+++ home/our_story.php
@@ -4,4 +4,10 @@
       <p>The history of The Hideaway cafe is a long and intriguing one.  First opened in 1988 by itës founder Jeff Yeh, The Hideaway cafe prides itself in providing quality food and service.  One of the proudest achievements of The Hideaway coffee and cafe was winning the gold medal for the 2016 annual coffe conferences.  With a wide variety of food and drinks, The Hideaway is sure to sate anyoneës hunger.  Vegetarian and gluten free options are available.</p>
       <button>Learn More</button>
     </div>
+
+    <div class="col-sm-4 col-sm offset 1">
+      <div class="thumbnail">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/hideaway_snacks.png" alt="cupcakes">
+      </div>
+    </div>
 </section>
