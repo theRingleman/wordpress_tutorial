77a92417b4c2464095cd9fbb6d72ae67a06e21ce
theRingleman <sam.ringleman@gmail.com>
2016-11-04 11:38:21 -0600

    make the hero section dynamic

--- home/hero.php
+++ home/hero.php
@@ -1,8 +1,17 @@
+<?php
+  // Advanced Custom Fields
+
+  $hero_logo = get_field("hero_logo");
+  $hero_text = get_field("hero_text");
+?>
+
 <section class="hero row">
   <?php get_template_part("navigation"); ?>
 
   <div class="hero-logo">
-    <h1>Welcome To</h1>
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-white.png" alt="" class="center-block">
+    <h1><?php echo $hero_text ?></h1>
+    <?php if(!empty($hero_logo)) : ?>
+      <img src="<?php echo $hero_logo['url']; ?>" alt="<?php echo $hero_logo['alt']; ?>" class="center-block">
+    <?php endif ?>
   </div>
 </section>
