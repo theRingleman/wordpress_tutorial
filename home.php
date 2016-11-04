330708e76621da305f9888ee98809728c3efadac
theRingleman <sam.ringleman@gmail.com>
2016-11-03 20:53:13 -0600

    add the home page template and update the navigation

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+  /*
+  Template Name: Home
+  */
+?>
+
+<?php get_header(); ?>
+<?php get_template_part("home/layout"); ?>
+<?php get_footer(); ?>
--- navigation.php
+++ navigation.php
@@ -1,4 +1,4 @@
-<?php if (is_home()) {
+<?php if (is_front_page()) {
   echo '<div class="navigation">';
 } else {
   echo '<div class="primary-navigation">';
